5c8ee1e766909839b53efa3e220bec233148fe9a
백호동 <ghehd2311@gmail.com>
2020-08-15 20:16:43 +0900

    Mutation 정의

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,16 @@
 //쿼리를 해결하는 파일
 //여기서 다른 데이터베이스에도 갈 수 있고 메모리나 다른 API로도 갈 수 있음
-import { people, getById } from './db';
+import { getMovies, getById } from './db';
 
 const resolvers = {
   Query: {
-    people: () => people,
-    // person: () => getById(),
-    person: (_, { id }) => getById(id),
+    movies: () => getMovies(),
+    movie: (_, { id }) => getById(id),
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => {
+      console.log(name, score);
+    },
   },
 };
 
