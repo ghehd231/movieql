4d47a666d3c1662bb163767c8e7aadcbdda5a6ca
백호동 <ghehd2311@gmail.com>
2020-08-15 20:41:06 +0900

    Mutation #2 영화 추가하기

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,6 @@
 //쿼리를 해결하는 파일
 //여기서 다른 데이터베이스에도 갈 수 있고 메모리나 다른 API로도 갈 수 있음
-import { getMovies, getById } from './db';
+import { getMovies, getById, addMovie } from './db';
 
 const resolvers = {
   Query: {
@@ -8,9 +8,7 @@ const resolvers = {
     movie: (_, { id }) => getById(id),
   },
   Mutation: {
-    addMovie: (_, { name, score }) => {
-      console.log(name, score);
-    },
+    addMovie: (_, { name, score }) => addMovie(name, score),
   },
 };
 
