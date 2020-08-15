7b244a859782911a620d611be7c82f401f4483ad
백호동 <ghehd2311@gmail.com>
2020-08-15 18:05:31 +0900

    스키마 확장

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,15 @@
 //쿼리를 해결하는 파일
 //여기서 다른 데이터베이스에도 갈 수 있고 메모리나 다른 API로도 갈 수 있음
+
+const ghehd = {
+  name: '호동',
+  age: 27,
+  gender: 'male',
+};
 const resolvers = {
   Query: {
     //스케마에 있는 name에 대한 해결
-    name: () => '백호동',
+    person: () => ghehd,
   },
 };
 
