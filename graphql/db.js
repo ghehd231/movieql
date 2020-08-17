1a5a562a9fbdb33b41207779d7b0368eaf14fc25
백호동 <ghehd2311@gmail.com>
2020-08-17 17:13:03 +0900

    getMovies and getMovie. Axios. Async

--- graphql/db.js
+++ graphql/db.js
@@ -1,14 +1,27 @@
-import fetch from 'node-fetch';
-const API_URL = 'https://yts.mx/api/v2/list_movies.json?';
+import axios from 'axios';
+const LIST_MOVIES_URL = 'https://yts.am/api/v2/list_movies.json?';
+const MOVIE_DETAILS_URL = 'https://yts.am/api/v2/movie_details.json';
 
-export const getMovies = (limit, rating) => {
+export const getMovies = async (limit, rating) => {
   // fetch(`${API_URL}`).then(res => res.json()).then(json => json.data.movies);
-  let REQEUST_URL = API_URL;
+  let REQEUST_URL = LIST_MOVIES_URL;
   if (limit > 0) {
     REQEUST_URL += `limit=${limit}`;
   }
   if (rating > 0) {
     REQEUST_URL += `&minimum_rating=${rating}`;
   }
-  return fetch(REQEUST_URL).then(res => res.json()).then(json => json.data.movies);
+
+  const { data: { data: { movies } } } = await axios(REQEUST_URL);
+
+  return movies;
+};
+
+export const getMovie = async id => {
+  const { data: { data: { movie } } } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movie;
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,11 @@
 //쿼리를 해결하는 파일
 //여기서 다른 데이터베이스에도 갈 수 있고 메모리나 다른 API로도 갈 수 있음
-import { getMovies } from './db';
+import { getMovies, getMovie } from './db';
 
 const resolvers = {
   Query: {
     movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
   },
 };
 
